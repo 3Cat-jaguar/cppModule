d3e04db073b919eb6c353059a7126885dfffbf2e
ylee <ylee@student.42seoul.kr>
2021-12-31 17:10:23 +0900

    change setting

--- module02/ex00/Fixed.hpp
+++ module02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 17:36:33 by ylee              #+#    #+#             */
-/*   Updated: 2021/12/27 18:09:12 by ylee             ###   ########.fr       */
+/*   Updated: 2021/12/31 17:08:34 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@
 class	Fixed
 {
 	private:
-		int			fixedValue;
+		int					fixedValue;
 		static const int	fracBitNum = 8;
 	public:
 		Fixed(void);
 		Fixed(Fixed& otherObj);
 		~Fixed(void);
 		Fixed&	operator=(Fixed& otherObj);
-		int	getRawBits(void);
+		int		getRawBits(void);
 		void	setRawBits(int const raw);
 };
 
