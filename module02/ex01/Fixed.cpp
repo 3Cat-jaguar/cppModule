8bc9265c0dd0aef714bb780f2f846bbfd29367c9
ylee <ylee@student.42seoul.kr>
2022-01-02 02:03:28 +0900

    fixed ex00 class, start ex01

--- module02/ex01/Fixed.cpp
+++ module02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 17:36:33 by ylee              #+#    #+#             */
-/*   Updated: 2022/01/01 22:31:39 by ylee             ###   ########.fr       */
+/*   Updated: 2022/01/01 22:33:09 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 Fixed::Fixed() : fixedValue(0)
 {
 	std::cout << "Default contructor called\n";
+}
+
+Fixed::Fixed(const int i)
+{
+	fixedValue = i << fracBitNum;
+	std::cout << "Int constructor called\n";
+}
+
+Fixed::Fixed(const float f)
+{
+	fixedValue = (int)roundf(f * (float)(1 << fracBitNum));
+	std::cout << "Float constructor called\n";
 }
 
 Fixed::Fixed(const Fixed& otherObj)
@@ -37,7 +49,6 @@ Fixed&	Fixed::operator=(const Fixed& otherObj)
 
 int		Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called\n";
 	return (this->fixedValue);
 }
 
@@ -45,3 +56,23 @@ void	Fixed::setRawBits(int const raw)
 {
 	fixedValue = raw;
 }
+
+float	Fixed::toFloat(void) const
+{
+	float	i = (float)fixedValue;
+	i = i / (float)(1 << fracBitNum);
+	return (i);
+}
+
+int		Fixed::toInt(void) const
+{
+	float i = this->toFloat();
+	return ((int)roundf(i));
+}
+
+std::ostream&	operator<<(std::ostream& out, const Fixed& obj)
+{
+	out << obj.toFloat();
+	return out ;
+}
+
--- module02/ex01/Fixed.hpp
+++ module02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: ylee <ylee@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 17:36:33 by ylee              #+#    #+#             */
-/*   Updated: 2022/01/01 22:23:48 by ylee             ###   ########.fr       */
+/*   Updated: 2022/01/01 22:32:56 by ylee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FIXED_HPP
 
 # include <iostream>
+# include <cmath>
 
 class	Fixed
 {
@@ -22,11 +23,17 @@ class	Fixed
 		static const int	fracBitNum = 8;
 	public:
 		Fixed(void);
+		Fixed(const int i);
+		Fixed(const float f);
 		Fixed(const Fixed& otherObj);
 		~Fixed(void);
 		Fixed&	operator=(const Fixed& otherObj);
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
 };
+
+std::ostream&	operator<<(std::ostream& out, const Fixed& obj);
 
 #endif
